28800cf1d8d3d05b6ee0a65bf80b45a484147ffe
jaunnnngs <ppepelo2@naver.com>
2021-07-21 17:59:44 +0900

    20210721
    20210721

--- STUN_TEAM/StunProjects/src/main/java/util/st_DatabaseUtil.java
+++ STUN_TEAM/StunProjects/src/main/java/util/st_DatabaseUtil.java
@@ -9,9 +9,9 @@ public class st_DatabaseUtil {
 		// 데이터베이스와 연결상태 관리
 		try {
 			//String dbURL = "jdbc:mysql://localhost:3306/TUTORIAL?useSSL=false&serverTimezone=UTC";
-			String dbURL = "jdbc:mysql://localhost:3306/hack_db?useUnicode=true&characterEncoding=utf-8&serverTimezone=UTC";
+			String dbURL = "jdbc:mysql://192.168.0.27:3306/hack_db?useUnicode=true&characterEncoding=utf-8&serverTimezone=UTC";
 
-			String dbID = "root";
+			String dbID = "HackUser";
 			String dbPassword = "1234"; //1234
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			return DriverManager.getConnection(dbURL, dbID, dbPassword);
