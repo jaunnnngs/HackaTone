2b1223075d9c62d9ee535c5fa9470ce1abfc9d54
ohnchangmin <ok_ddol@naver.com>
2021-08-12 11:17:40 +0900

    db완성본

--- reservationDB/Reservation.java
+++ reservationDB/Reservation.java
@@ -2,9 +2,9 @@ package reservation;
 
 public class Reservation {
 	
-	
 	public ReservationDTO getResverationInfo(String name, String tel, String number, String storeName, String reservationDate) {
 		ReservationDTO reDto = new ReservationDTO(name, tel, number, storeName, reservationDate);
 		return reDto;
 	}
 }
+ 
--- reservationDB/st_UserDTO.java
+++ reservationDB/st_UserDTO.java
@@ -39,3 +39,4 @@ public class st_UserDTO {
 	}
 
 }
+
